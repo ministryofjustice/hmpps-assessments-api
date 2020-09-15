0dfdfe36724b6843afba53c6ceb444163e7aaade
Jez Higgins <jez@jezuk.co.uk>
2020-09-15 11:02:49 +0100

    Creating an assessment when one exists
    Added test - attempting to create a second assessment for the
    supervision should not create a new assessment but instead return the
    current assessment.

--- src/test/kotlin/uk/gov/justice/digital/assessments/controller/AssessmentControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/assessments/controller/AssessmentControllerTest.kt
@@ -10,22 +10,18 @@ import java.time.LocalDateTime
 
 @AutoConfigureWebTestClient
 class AssessmentControllerTest : IntegrationTest() {
-
     private val supervisionId = "SUPERVISION1"
 
     @Test
     fun `access forbidden when no authority`() {
-
         webTestClient.get().uri("/assessments/supervision/$supervisionId")
                 .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isUnauthorized
     }
 
-
     @Test
     fun `create a new assessment returns assessment`() {
-
         webTestClient.post().uri("/assessments/supervision")
                 .bodyValue(CreateAssessmentDto("SupervisionId"))
                 .headers(setAuthorisation())
@@ -40,4 +36,30 @@ class AssessmentControllerTest : IntegrationTest() {
                 }
     }
 
+    @Test
+    fun `trying to create an assessment when one already exists returns the existing assessment`() {
+        // create assessment
+        val assessment = webTestClient.post().uri("/assessments/supervision")
+                .bodyValue(CreateAssessmentDto("ExistingSupervisionId"))
+                .headers(setAuthorisation())
+                .exchange()
+                .expectStatus().isOk
+                .expectBody<AssessmentDto>()
+                .returnResult()
+                .responseBody
+
+        // try and create another
+        val existing = webTestClient.post().uri("/assessments/supervision")
+                .bodyValue(CreateAssessmentDto("ExistingSupervisionId"))
+                .headers(setAuthorisation())
+                .exchange()
+                .expectStatus().isOk
+                .expectBody<AssessmentDto>()
+                .returnResult()
+                .responseBody
+
+        assertThat(existing.assessmentId).isEqualTo(assessment.assessmentId)
+        assertThat(existing.supervisionId).isEqualTo(assessment.supervisionId)
+        assertThat(existing.createdDate).isEqualTo(assessment.createdDate)
+    }
 }
