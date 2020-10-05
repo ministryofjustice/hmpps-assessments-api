3a8feef428c4a750b375201797e83d7daedef159
Jez Higgins <jez@jezuk.co.uk>
2020-10-05 12:34:35 +0100

    Layer 3 and BCST groups

--- src/main/resources/db/migration/V1_5__further_reference_data.sql
+++ src/main/resources/db/migration/V1_5__further_reference_data.sql
@@ -1,3 +1,5 @@
+-- noinspection SqlResolveForFile
+
 INSERT INTO question_schema (question_schema_id, question_schema_uuid, question_code, oasys_question_code, question_start, question_end, answer_type, question_text, question_help_text)
 VALUES  (201, '11111111-1111-1111-1111-111111111201', 'surname', 'OL3 CI1', '2019-11-14 08:11:53.177108', null, 'freetext', 'Surname', null),
         (202, '11111111-1111-1111-1111-111111111202', 'forename', 'OL3 CI2', '2019-11-14 08:11:53.177108', null, 'freetext', 'Forename', null),
@@ -7,4 +9,10 @@ VALUES  (201, '11111111-1111-1111-1111-111111111201', 'surname', 'OL3 CI1', '201
         (206, '11111111-1111-1111-1111-111111111206', 'dob_alias', 'OL3 CI6', '2019-11-14 08:11:53.177108', null, 'freetext', 'Date of Birth (Aliases)', null),
         (207, '11111111-1111-1111-1111-111111111207', 'gender', 'OL3 CI7', '2019-11-14 08:11:53.177108', null, 'freetext', 'Gender', null),
         (208, '11111111-1111-1111-1111-111111111208', 'religion', 'OL3 CI8', '2019-11-14 08:11:53.177108', null, 'freetext', 'Religion', null);
+
+INSERT INTO grouping (group_id, group_uuid, heading, subheading, help_text)
+VALUES  (201, '22222222-2222-2222-2222-222222222201', 'Layer 3', null, null),
+        (202, '22222222-2222-2222-2222-222222222202', 'Case Identification', null, null),
+        (203, '22222222-2222-2222-2222-222222222203', 'BCST', null, null),
+        (204, '22222222-2222-2222-2222-222222222204', 'Case Identification', null, null);
 
