81f286183d4314efe4925faa567b621f5162b5f9
Jez Higgins <jez@jezuk.co.uk>
2020-10-12 15:07:33 +0100

    populate contentCount, groupCount, questionCount in GroupSummaryEntity

--- src/main/kotlin/uk/gov/justice/digital/assessments/jpa/entities/GroupSummaryEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/jpa/entities/GroupSummaryEntity.kt
@@ -1,7 +1,10 @@
 package uk.gov.justice.digital.assessments.jpa.entities;
 
+import java.util.*
+
 interface GroupSummaryEntity {
     val heading: String
+    val contentCount: Long
     val groupCount: Long
     val questionCount: Long
 }
--- src/main/kotlin/uk/gov/justice/digital/assessments/jpa/repositories/QuestionGroupRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/jpa/repositories/QuestionGroupRepository.kt
@@ -13,9 +13,12 @@ interface QuestionGroupRepository: JpaRepository<QuestionGroupEntity, String> {
     fun findByGroupGroupUuid(groupId: UUID): Collection<QuestionGroupEntity>?
 
     @Query(value = "select (g.heading\\:\\:varchar) as heading, " +
-                   "-1 as groupCount, " +
-                   "0 as questionCount " +
-                   "from grouping g ",
+                   "count(qg.content_uuid) as contentCount, " +
+                   "count(case when qg.content_type = 'grouping' then qg.content_uuid end) as groupCount, " +
+                   "count(case when qg.content_type = 'question' then qg.content_uuid end) as questionCount, " +
+                   "from question_group qg " +
+                   "left join grouping g on qg.group_uuid = g.group_uuid " +
+                   "group by g.group_uuid, g.heading ",
            nativeQuery = true)
     fun listGroups(): Collection<GroupSummaryEntity>
 }
