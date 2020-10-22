88544f1cae2ea5fdcaa1b845a704f76f25b33a6f
Jez Higgins <jez@jezuk.co.uk>
2020-10-22 11:48:15 +0100

    Add a answer group to Accommodation question

--- src/main/resources/db/migration/V1_4__test_reference_data.sql
+++ src/main/resources/db/migration/V1_4__test_reference_data.sql
@@ -31,18 +31,18 @@ VALUES  (1, '33333333-3333-3333-3333-333333333331', '11111111-1111-1111-1111-111
         (6, '33333333-3333-3333-3333-333333333336', '11111111-1111-1111-1111-111111111116', 'question','22222222-2222-2222-2222-222222222223', '2.3', 'mandatory', null);
 
 
-INSERT INTO question_schema (question_schema_id, question_schema_uuid, question_code, oasys_question_code, question_start, question_end, answer_type, question_text, question_help_text)
-VALUES  (201, '11111111-1111-1111-1111-111111111201', 'surname', 'OL3 CI1', '2019-11-14 08:11:53.177108', null, 'freetext', 'Surname', null),
-(202, '11111111-1111-1111-1111-111111111202', 'forename', 'OL3 CI2', '2019-11-14 08:11:53.177108', null, 'freetext', 'Forename', null),
-(203, '11111111-1111-1111-1111-111111111203', 's_alias', 'OL3 CI3', '2019-11-14 08:11:53.177108', null, 'freetext', 'Surname Aliases', null),
-(204, '11111111-1111-1111-1111-111111111204', 'f_alias', 'OL3 CI4', '2019-11-14 08:11:53.177108', null, 'freetext', 'Forename Aliases', null),
-(205, '11111111-1111-1111-1111-111111111205', 'dob', 'OL3 CI5', '2019-11-14 08:11:53.177108', null, 'freetext', 'Date of Birth', null),
-(206, '11111111-1111-1111-1111-111111111206', 'dob_alias', 'OL3 CI6', '2019-11-14 08:11:53.177108', null, 'freetext', 'Date of Birth (Aliases)', null),
-(207, '11111111-1111-1111-1111-111111111207', 'gender', 'OL3 CI7', '2019-11-14 08:11:53.177108', null, 'freetext', 'Gender', null),
-(208, '11111111-1111-1111-1111-111111111208', 'religion', 'OL3 CI8', '2019-11-14 08:11:53.177108', null, 'freetext', 'Religion', null),
+INSERT INTO question_schema (question_schema_id, question_schema_uuid, question_code, oasys_question_code, question_start, question_end, answer_type, answer_schema_group_uuid, question_text, question_help_text)
+VALUES  (201, '11111111-1111-1111-1111-111111111201', 'surname', 'OL3 CI1', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Surname', null),
+(202, '11111111-1111-1111-1111-111111111202', 'forename', 'OL3 CI2', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Forename', null),
+(203, '11111111-1111-1111-1111-111111111203', 's_alias', 'OL3 CI3', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Surname Aliases', null),
+(204, '11111111-1111-1111-1111-111111111204', 'f_alias', 'OL3 CI4', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Forename Aliases', null),
+(205, '11111111-1111-1111-1111-111111111205', 'dob', 'OL3 CI5', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Date of Birth', null),
+(206, '11111111-1111-1111-1111-111111111206', 'dob_alias', 'OL3 CI6', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Date of Birth (Aliases)', null),
+(207, '11111111-1111-1111-1111-111111111207', 'gender', 'OL3 CI7', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Gender', null),
+(208, '11111111-1111-1111-1111-111111111208', 'religion', 'OL3 CI8', '2019-11-14 08:11:53.177108', null, 'freetext', null, 'Religion', null),
 
 -- Layer 3 Accommodation
-(231, '11111111-1111-1111-1111-111111111231', 'no_fixed_abode', 'OL3 3.3', '2019-11-14 08:11:53.177108', null, 'freetext', 'Currently of no fixed abode or in transient accomodation', null);
+(231, '11111111-1111-1111-1111-111111111231', 'no_fixed_abode', 'OL3 3.3', '2019-11-14 08:11:53.177108', null, 'freetext', '44444444-4444-4444-5555-444444444444', 'Currently of no fixed abode or in transient accomodation', null);
 
 INSERT INTO grouping (group_id, group_uuid, group_code, heading, subheading, help_text, group_start, group_end)
 VALUES  (201, '22222222-2222-2222-2222-222222222201', 'oasys', 'Layer 3', null, null, '2019-11-14 08:11:53.177108', null),
