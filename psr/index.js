d34bcc24717b1a71eb55d6c1998dcc721e65cef8
Jez Higgins <jez@jezuk.co.uk>
2020-11-30 16:49:09 +0000

    generate question groups

--- psr/index.js
+++ psr/index.js
@@ -15,6 +15,7 @@ const records = all_records.filter(record => record.join('')) // remove lines wi
 
 const groups = []
 const questions = []
+const questionGroups = []
 const psr = addGrouping(['Pre-Sentence Report'])
 
 let currentGroup = null
@@ -24,17 +25,20 @@ for (let index = 1; index !== records.length; ++index) {
 
   if (isGroup(record)) {
     currentGroup = addGrouping(record)
+    addQuestionGroup(currentGroup.group_uuid, 'group', psr.group_uuid)
     continue
   }
 
   if (!currentGroup)
     continue
 
-  addQuestion(record)
+  const question = addQuestion(record)
+  addQuestionGroup(question.question_schema_uuid, 'question', currentGroup.group_uuid)
 }
 
 console.log(groupingSql())
 console.log(questionsSql())
+console.log(questionGroupSql())
 
 function isGroup(record) {
   const cleaned = record.filter(f => f)
@@ -56,7 +60,7 @@ function addGrouping(record) {
 
 function addQuestion(record) {
   const title = record[0]
-  const question_text = record[9]
+  const question_text = record[9].replace(/'/g, "''")
   const answer_type = 'freetext' // record[11]
   const oasys_question_code = record[17] || null
   const question = {
@@ -70,6 +74,20 @@ function addQuestion(record) {
   }
   questions.push(question)
   return question
+}
+
+function addQuestionGroup(content_uuid, content_type, group_uuid) {
+  const questionGroup = {
+    question_group_id: questionGroups.length + 101,
+    question_group_uuid: uuid(),
+    content_uuid: content_uuid,
+    content_type: content_type,
+    group_uuid: group_uuid,
+    display_order: questionGroups.filter(qg => qg.group_uuid === group_uuid).length + 1,
+    mandatory: 'yes',
+    validation: null
+  }
+  questionGroups.push(questionGroup)
 }
 
 function insertSql(table, fields) {
@@ -104,5 +122,13 @@ function questionsSql() {
     'question_schema',
     ['question_schema_id', 'question_schema_uuid', 'question_code', 'oasys_question_code', 'question_start', 'question_end', 'answer_type', 'answer_schema_group_uuid', 'question_text', 'question_help_text'],
     questions
+  )
+}
+
+function questionGroupSql() {
+  return tableSql(
+    'question_group',
+    ['question_group_id', 'question_group_uuid', 'content_uuid', 'content_type', 'group_uuid', 'display_order', 'mandatory', 'validation'],
+    questionGroups
   )
 }
