8639481e53bcba89882465f9189e2c6bda489ca1
Jez Higgins <jez@jezuk.co.uk>
2020-12-10 12:01:24 +0000

    Rename offender table to subject
    They might not be an offender :)

--- src/main/resources/db/migration/V1_3__subject.sql
+++ src/main/resources/db/migration/V1_3__subject.sql
@@ -1,12 +1,12 @@
 -- noinspection SqlResolveForFile
 
-CREATE TABLE IF NOT EXISTS offender
+CREATE TABLE IF NOT EXISTS subject
 (
-    offender_id     VARCHAR(50)     PRIMARY KEY,
-    offender_uuid   uuid            UNIQUE,
+    subject_id      VARCHAR(50)     PRIMARY KEY,
+    subject_uuid    uuid            UNIQUE,
     source_id       TEXT            NOT NULL,
     source          TEXT            NOT NULL,
-    assessment_uuid   UUID            NOT NULL,
+    assessment_uuid UUID            NOT NULL,
     FOREIGN KEY (assessment_uuid)
         REFERENCES assessment(assessment_uuid)
 );
