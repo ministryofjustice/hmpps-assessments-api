5501e1c6bd3204666084bfe7e3c2af4f41c06575
Jez Higgins <jez@jezuk.co.uk>
2021-01-08 10:15:46 +0000

    Hooked up simple QuestionDependencyService

---
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/QuestionDependencyService.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.assessments.services
+
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.assessments.jpa.entities.QuestionDependencyEntity
+import uk.gov.justice.digital.assessments.jpa.repositories.QuestionDependencyRepository
+import java.util.*
+
+@Service
+class QuestionDependencyService(
+        private val questionDependencyRepository: QuestionDependencyRepository
+) {
+    fun dependencies(): QuestionDependencies {
+        val qd = questionDependencyRepository.findAll()
+
+        return QuestionDependencies(qd)
+    }
+}
+
+class QuestionDependencies(questionDeps: Collection<QuestionDependencyEntity>) {
+    private val subjects = questionDeps.map { it.subjectQuestionUuid }
+    private val triggers = questionDeps.associateBy(
+            { Pair(it.triggerQuestionUuid, it.triggerAnswerValue) },
+            { it.subjectQuestionUuid }
+    )
+
+    fun hasDependency(subjectUuid: UUID): Boolean = subjects.contains(subjectUuid)
+    fun triggersDependency(triggerUuid: UUID, answerValue: String): UUID? =
+            triggers.get(Pair(triggerUuid, answerValue))
+}
