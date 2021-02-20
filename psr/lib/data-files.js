19d86e872347da8ee32e711c8136e6b37adabc09
Jez Higgins <jez@jezuk.co.uk>
2021-02-20 10:25:47 +0000

    Split uuid files out from data files.
    Data files are provided by us actual humans while the uuids are
    generated and updated by the program as it runs.

--- psr/lib/data-files.js
+++ psr/lib/data-files.js
@@ -13,24 +13,38 @@ function loadFile(
 function dataPath(filename) {
   return path.resolve(__dirname, '..', 'data', filename)
 }
+function uuidPath(filename) {
+  return path.resolve(__dirname, '..', 'data', 'uuids', filename)
+}
 
-function loadDataFile(filename) {
-  const contents = loadFile(dataPath(filename))
+function load(filepath) {
+  const contents = loadFile(filepath)
     .map(fields => fields.slice(1)) // title is just for information
   return Object.fromEntries(contents)
 }
 
-function appendDataFile(filename, title, ref, value) {
+function loadDataFile(filename) {
+  return load(dataPath(filename))
+}
+
+function loadUuidFile(filename) {
+  return load(uuidPath(filename))
+}
+
+function appendUuidFile(filename, title, ref, value) {
   fs.appendFileSync(
-    dataPath(filename),
+    uuidPath(filename),
     `${title.replace(/\r?\n|\r/g, '')},${ref},${value}\n`
   )
 }
 
 module.exports = {
   loadFile: loadFile,
+  UuidFile: {
+    load: loadUuidFile,
+    append: appendUuidFile
+  },
   DataFile: {
-    load: loadDataFile,
-    append: appendDataFile
+    load: loadDataFile
   }
 }
