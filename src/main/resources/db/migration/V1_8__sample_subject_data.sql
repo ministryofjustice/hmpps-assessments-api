aae8d5db56104dab1ee4cc8aac768c4f7fdcb74f
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-03-05 09:29:30 +0000

    :white_check_mark: update test data in flyway for court subject

--- src/main/resources/db/migration/V1_8__sample_subject_data.sql
+++ src/main/resources/db/migration/V1_8__sample_subject_data.sql
@@ -1,4 +1,7 @@
 
+INSERT INTO assessment (assessment_uuid, supervision_id, created_date, completed_date )
+VALUES ('db9dcd12-2e47-4b00-b8e9-44ddf38b9d6a', '12345', '2019-11-14 08:11:53.177108', null);
+
 
 INSERT INTO subject (subject_id, subject_uuid, source, source_id, name, pnc, crn, date_of_birth, created_date, assessment_uuid) VALUES
-(1, 'a4e73a2c-3f1c-4f83-88b6-dd3ce1b78530', 'COURT', 'B10JQ|160346204', 'Arthur Morgan', '2004/0046583U', 'X346204', '1975-01-01', '2019-11-14 08:30', 'e69a61ff-7395-4a12-b434-b1aa6478aded');
+(1, 'a4e73a2c-3f1c-4f83-88b6-dd3ce1b78530', 'COURT', 'B10JQ|160346204', 'Arthur Morgan', '2004/0046583U', 'X346204', '1975-01-01', '2019-11-14 08:30', 'db9dcd12-2e47-4b00-b8e9-44ddf38b9d6a');
--- src/test/resources/subject/after-test.sql
+++ src/test/resources/subject/after-test.sql
@@ -3,4 +3,3 @@
 delete from assessed_episode where true;
 delete from subject where true;
 delete from assessment where true;
-
