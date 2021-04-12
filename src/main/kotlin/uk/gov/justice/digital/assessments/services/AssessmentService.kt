ce00bf3c7df9937d30992d24c2952113d4bdf56a
wood-n <Natalie.wood@digital.justice.gov.uk>
2021-04-12 13:57:19 +0100

    Arn complete episode (#127)
    * :sparkles: add complete episode functionality to call updates service to trigger OASys complete assessment.
    
    * :bug: fix logic to not save the closed episode if the updates service returns errors

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentService.kt
@@ -267,7 +267,7 @@ class AssessmentService(
       return AssessmentEpisodeDto.from(episode, null)
     }
     val oasysResult = completeOASysAssessment(offenderPk, episode)
-    if (oasysResult == null) {
+    if (oasysResult == null || oasysResult.hasErrors()) {
       log.info("Unable to close episode ${episode.episodeUuid} for assessment ${episode.assessment?.assessmentUuid} with OASys restclient")
     } else {
       episode.close()
