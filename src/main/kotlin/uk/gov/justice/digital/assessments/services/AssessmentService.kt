419e2a2cb978c19b6dc561dd3a1bfd222ff55618
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-04-12 15:12:13 +0100

    :bug: fix boolean logic in if

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentService.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentService.kt
@@ -293,12 +293,6 @@ class AssessmentService(
     return AssessmentEpisodeUpdateErrors.mapOasysErrors(episode, null, oasysUpdateResult)
   }
 
-
-
-
-
-
-
   private fun getEpisode(episodeUuid: UUID, assessmentUuid: UUID): AssessmentEpisodeEntity {
     return getAssessmentByUuid(assessmentUuid).episodes.firstOrNull { it.episodeUuid == episodeUuid }
       ?: throw EntityNotFoundException("No Episode $episodeUuid for $assessmentUuid")
