1a610e337fbb103c868f8fdaa811e8c9aaf73e5f
adelaidaa <adelaida.aj@gmail.com>
2021-07-08 16:52:31 +0200

    klint

--- src/main/kotlin/uk/gov/justice/digital/assessments/jpa/entities/AssessmentSchemaGroupsEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/jpa/entities/AssessmentSchemaGroupsEntity.kt
@@ -4,8 +4,6 @@ import java.io.Serializable
 import java.util.UUID
 import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.EnumType
-import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
