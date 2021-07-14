a9a0e66362edeb1e45d96df6b160c2854c8dcd12
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-07-14 17:26:38 +0100

    add offender stub dto

---
+++ src/main/kotlin/uk/gov/justice/digital/assessments/utils/OffenderStubDto.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.assessments.utils
+
+data class OffenderStubDto(
+  val crn: String?,
+  val pnc: String?,
+  val familyName: String?,
+  val forename1: String?,
+  val areaCode: String?,
+)
