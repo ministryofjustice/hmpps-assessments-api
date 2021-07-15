19d60e2235b33529275b7ae01096a860462d4fcb
Lucas Cairns <lucas.cairns@gmail.com>
2021-07-15 16:30:53 +0100

    Add method to extract answers

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/PredictorService.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/PredictorService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.assessments.services
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.assessments.api.AnswerDto
 import uk.gov.justice.digital.assessments.api.AnswersDto
 import uk.gov.justice.digital.assessments.jpa.entities.AssessmentSchemaCode
 import uk.gov.justice.digital.assessments.jpa.entities.PredictorFieldMapping
@@ -24,7 +25,9 @@ class PredictorService(
     log.info("Found ${predictors.size} predictors for assessment type $assessmentSchemaCode")
 
     return predictors.map {
-      if (hasRequiredFieldsForScore(it.predictorFields.toList(), episodeAnswers)) {
+      val predictorFields = it.predictorFields.toList()
+      val extractedAnswers = extractAnswers(it.predictorFields.toList(), episodeAnswers)
+      if (predictorFields.isNotEmpty() && predictorFields.size == extractedAnswers.size) {
         // Request predictor score and return PredictorResult
         1
       } else {
@@ -34,14 +37,16 @@ class PredictorService(
     }
   }
 
-  private fun hasRequiredFieldsForScore(
+  private fun extractAnswers(
     predictorFields: List<PredictorFieldMapping>,
     answers: Map<UUID, AnswersDto>
-  ): Boolean {
-    val numberOfAnsweredPredictorFields = predictorFields.fold(0) {
-      total, field -> if (answers[field.questionSchema.questionSchemaUuid] != null) total + 1 else total
-    }
-
-    return predictorFields.isNotEmpty() && numberOfAnsweredPredictorFields == predictorFields.size
+  ): Map<UUID, Collection<AnswerDto>> {
+    return predictorFields
+      .map { it.questionSchema.questionSchemaUuid }
+      .associate { questionSchemaUuid ->
+        val questionAnswer = answers[questionSchemaUuid]
+        (questionSchemaUuid to questionAnswer?.answers.orEmpty())
+      }
+      .filterValues { it.isNotEmpty() }
   }
 }
