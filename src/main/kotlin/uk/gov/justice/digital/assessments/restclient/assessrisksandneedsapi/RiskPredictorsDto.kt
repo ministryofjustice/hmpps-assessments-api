d88ac5b1640e53b1f60faeb7a90041d839f4603d
adelaidaa <adelaida.aj@gmail.com>
2021-08-06 14:14:59 +0100

    fix score level for ARN

--- src/main/kotlin/uk/gov/justice/digital/assessments/restclient/assessrisksandneedsapi/RiskPredictorsDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/restclient/assessrisksandneedsapi/RiskPredictorsDto.kt
@@ -19,7 +19,7 @@ data class Score(
 )
 
 enum class ScoreLevel(val type: String) {
-  LOW("Low"), MEDIUM("Medium"), HIGH("High"), NOT_APPLICABLE("Not Applicable");
+  LOW("Low"), MEDIUM("Medium"), HIGH("High"), VERY_HIGH("Very High"), NOT_APPLICABLE("Not Applicable");
 
   companion object {
     fun findByType(type: String): ScoreLevel? {
