17b18fd5d910c3e20af1d0276e97202cf34909ae
adelaidaa <adelaida.aj@gmail.com>
2021-08-09 11:00:21 +0100

    trying to fix duplicated identity issue

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -2,8 +2,8 @@ INSERT INTO assessment (assessment_uuid, created_date, completed_date )
 VALUES ('fb6b7c33-07fc-4c4c-a009-8d60f66952c4', '2019-11-14 08:11:53.177108', null)
 ON CONFLICT DO NOTHING;
 
-insert into subject (subject_id, subject_uuid, source, source_id, name, pnc, crn, date_of_birth, created_date, assessment_uuid, OASYS_OFFENDER_PK) values
-(1, 'fac28f68-0012-46b9-8de8-6ff2bdbe1c22', 'DELIUS', 1, 'EleRgvLL JanRgvDD', null, 'X259950', '1977-08-15', '2021-07-12 16:42:06', 'fb6b7c33-07fc-4c4c-a009-8d60f66952c4', 7308807)
+insert into subject (subject_uuid, source, source_id, name, pnc, crn, date_of_birth, created_date, assessment_uuid, OASYS_OFFENDER_PK) values
+('fac28f68-0012-46b9-8de8-6ff2bdbe1c22', 'DELIUS', 1, 'EleRgvLL JanRgvDD', null, 'X259950', '1977-08-15', '2021-07-12 16:42:06', 'fb6b7c33-07fc-4c4c-a009-8d60f66952c4', 7308807)
 ON CONFLICT DO NOTHING;
 
 INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers, OASYS_SET_PK  )
@@ -16,8 +16,8 @@ INSERT INTO assessment (assessment_uuid, created_date, completed_date )
 VALUES ('6f3f2c4a-38ac-49ce-b790-70bc170fe553', '2021-01-01 08:11:53.177108', null)
 ON CONFLICT DO NOTHING;
 
-insert into subject (subject_id, subject_uuid, source, source_id, name, pnc, crn, date_of_birth, created_date, assessment_uuid) values
-(2, '087bdec0-98de-4c86-af3d-2d05ff978007', 'DELIUS', 1, 'EleRgvLL JanRgvDD', null, 'X259950', '1977-08-15', '2021-07-12 16:42:06', '6f3f2c4a-38ac-49ce-b790-70bc170fe553')
+insert into subject (subject_uuid, source, source_id, name, pnc, crn, date_of_birth, created_date, assessment_uuid) values
+('087bdec0-98de-4c86-af3d-2d05ff978007', 'DELIUS', 1, 'EleRgvLL JanRgvDD', null, 'X259950', '1977-08-15', '2021-07-12 16:42:06', '6f3f2c4a-38ac-49ce-b790-70bc170fe553')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers  )
