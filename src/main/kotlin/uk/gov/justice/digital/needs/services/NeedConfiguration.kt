9ef339c31e5136aff2855a5c521ac0d1554db23f
wood-n <Natalie.wood@digital.justice.gov.uk>
2021-08-16 16:44:36 +0100

    :fire: remove criminogenic needs endpoint (#251)

--- src/main/kotlin/uk/gov/justice/digital/needs/services/NeedConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.needs.services
-
-class NeedConfiguration(
-  val harmQuestion: String,
-  val reoffendingQuestion: String,
-  val lowScoreNeedQuestion: String,
-  val threshold: Int,
-  val thresholdQuestions: Set<String> // threshold questions are ints, values from a likert scale
-)
