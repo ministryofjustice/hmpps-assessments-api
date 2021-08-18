beec5918413664f1b089ae846186141db1127c56
adelaidaa <adelaida.aj@gmail.com>
2021-08-18 12:25:28 +0100

    point to right perpetrator question

--- src/main/resources/db/migration/V1_9__predictor_mappings.sql
+++ src/main/resources/db/migration/V1_9__predictor_mappings.sql
@@ -22,7 +22,7 @@ INSERT INTO predictor_field_mapping (predictor_mapping_uuid, question_schema_uui
 ('6a37b92f-f316-4e66-bb28-fc1c6d88b79b', '3211a668-8878-4e88-8457-8250bfe65aea', 'RSR', 'unemployed_on_release', false),
 ('b608c6fd-4b93-4581-886b-f5925f0d9b4d', '1970ba5e-91cb-4ad3-9f04-64d5b5b7157b', 'RSR', 'current_relationship_with_partner', false),
 ('89926828-7862-4b22-b02e-2e449a76c622', 'f04dd882-0a0d-49f5-9736-91eeadbff9e7', 'RSR', 'evidence_domestic_violence', false),
-('ca66afba-46c0-4be4-a23d-306bce15a4fd', '38b3a40a-df23-4ea1-872e-c04a8b03ee05', 'RSR', 'perpetrator_domestic_violence', false),
+('ca66afba-46c0-4be4-a23d-306bce15a4fd', 'f04dd882-0a0d-49f5-9736-91eeadbff9e7', 'RSR', 'perpetrator_domestic_violence', false),
 ('a4feacfe-6bda-41d6-806e-872c0b84189e', 'f0416e89-3a71-46d1-8fa2-aebd886dcb34', 'RSR', 'use_of_alcohol', false),
 ('8dea672f-5264-4973-94f4-9d10f1daf5f6', '574618c3-27f4-4dd2-94bb-6de74126ff22', 'RSR', 'binge_drinking', false),
 ('0365e911-b1bd-4271-b498-3657eda1c859', '5a90a38d-ee0a-4775-994c-addf3397b817', 'RSR', 'impulsivity_issues', false),
