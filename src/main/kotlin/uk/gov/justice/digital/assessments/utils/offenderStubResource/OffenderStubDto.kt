f2794ea513ff0cf3139f60e18f7f3d7bc2545e58
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-08-23 16:40:00 +0100

    :bug: allow offender stub to handle null crn values

--- src/main/kotlin/uk/gov/justice/digital/assessments/utils/offenderStubResource/OffenderStubDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/utils/offenderStubResource/OffenderStubDto.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.assessments.utils.offenderStubResource
 
 data class OffenderStubDto(
-  val crn: String,
+  val crn: String? = null,
   val pnc: String?,
   val familyName: String?,
   val forename1: String?,
