2791ef62cd52c9497ed0e0b52262301235f208ca
Lucas Cairns <lucas.cairns@gmail.com>
2021-08-26 15:06:02 +0100

    Add endpoint to get questions for an Assessment Schema code (#275)

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentSchemaService.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/AssessmentSchemaService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.assessments.services
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.assessments.api.GroupSectionsDto
 import uk.gov.justice.digital.assessments.api.GroupWithContentsDto
+import uk.gov.justice.digital.assessments.api.QuestionSchemaDto
 import uk.gov.justice.digital.assessments.jpa.entities.AssessmentSchemaCode
 import uk.gov.justice.digital.assessments.jpa.entities.OasysAssessmentType
 import uk.gov.justice.digital.assessments.jpa.entities.Predictor
@@ -26,6 +27,11 @@ class AssessmentSchemaService(
         ?: throw EntityNotFoundException("Assessment Schema not found for assessmentSchemaCode $assessmentSchemaCode")
 
     return questionService.getGroupContents(assessmentSchemaGroupUuid)
+  }
+
+  fun getQuestionsForSchemaCode(assessmentSchemaCode: AssessmentSchemaCode?) : List<QuestionSchemaDto> {
+    val assessmentSchema = getAssessmentSchema(assessmentSchemaCode)
+    return questionService.getAllGroupQuestionsByGroupCode(assessmentSchema.groupCode).map { QuestionSchemaDto.from(it) }
   }
 
   fun getAssessmentSchemaSummary(assessmentSchemaCode: AssessmentSchemaCode?): GroupSectionsDto {
