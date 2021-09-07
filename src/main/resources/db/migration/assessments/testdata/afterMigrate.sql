f2ea06d89178397ed2017843c8c89e58b2b794fe
adelaidaa <adelaida.aj@gmail.com>
2021-09-07 18:14:41 +0100

    extract test data for assessments and profile to only add in dev and test

--- src/main/resources/db/migration/assessments/testdata/afterMigrate.sql
+++ src/main/resources/db/migration/assessments/testdata/afterMigrate.sql
@@ -2,11 +2,11 @@ INSERT INTO assessment (assessment_uuid, created_date, completed_date )
 VALUES ('fb6b7c33-07fc-4c4c-a009-8d60f66952c4', '2021-01-01 08:11:53.177108', null)
 ON CONFLICT DO NOTHING;
 
-insert into subject (subject_uuid, source, source_id, name, pnc, crn, date_of_birth, gender, created_date, assessment_uuid, OASYS_OFFENDER_PK) values
+insert into subject (subject_uuid, source, source_id, name, pnc, crn, date_of_birth, gender, created_date, assessment_uuid, oasys_offender_pk) values
 ('fac28f68-0012-46b9-8de8-6ff2bdbe1c22', 'DELIUS', 1, 'Gary Hart', null, 'X259950', '1977-08-15', 'MALE', '2021-07-12 16:42:06', 'fb6b7c33-07fc-4c4c-a009-8d60f66952c4', 7308807)
 ON CONFLICT DO NOTHING;
 
-INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers, OASYS_SET_PK  )
+INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers, oasys_set_pk )
 VALUES ('7231b3e7-f002-4a53-b398-fcd34c1d3e97', 'AALONSO', '2021-01-01 08:11:53.177109', null, 'new episode', 'ROSH', 'fb6b7c33-07fc-4c4c-a009-8d60f66952c4','{}', 9519347)
 ON CONFLICT DO NOTHING;
 
