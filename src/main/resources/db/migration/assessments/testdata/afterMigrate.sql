8527728fa40f27167dd41587d0e48f33920ed7d1
adelaidaa <adelaida.aj@gmail.com>
2021-09-23 14:04:56 +0100

    fix merge

--- src/main/resources/db/migration/assessments/testdata/afterMigrate.sql
+++ src/main/resources/db/migration/assessments/testdata/afterMigrate.sql
@@ -10,7 +10,7 @@ ON CONFLICT DO NOTHING;
 
 INSERT INTO offence (offence_uuid, source, source_id, offence_code, code_description, offence_subcode, subcode_description, sentence_date)
 VALUES ('877323d1-1e0f-42f3-b5b5-f44b0e5bcb88', 'DELIUS', 1, '046', 'Stealing from shops and stalls (shoplifting)', '00', 'Stealing from shops and stalls (shoplifting)', '2021-07-12')
-    ON CONFLICT DO NOTHING;
+ON CONFLICT DO NOTHING;
 
 INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers, tables, oasys_set_pk, offence_uuid )
 VALUES ('7231b3e7-f002-4a53-b398-fcd34c1d3e97', 'AALONSO', '2021-01-01 08:11:53.177109', null, 'new episode', 'ROSH', 'fb6b7c33-07fc-4c4c-a009-8d60f66952c4','{}','{}', 9519347, '877323d1-1e0f-42f3-b5b5-f44b0e5bcb88')
@@ -27,11 +27,7 @@ ON CONFLICT DO NOTHING;
 
 INSERT INTO offence (offence_uuid, source, source_id, offence_code, code_description, offence_subcode, subcode_description, sentence_date)
 VALUES ('977323d1-1e0f-42f3-b5b5-f44b0e5bcb18', 'DELIUS', 1, '046', 'Stealing from shops and stalls (shoplifting)', '00', 'Stealing from shops and stalls (shoplifting)', '2021-07-12')
-    ON CONFLICT DO NOTHING;
-
-INSERT INTO offence (offence_uuid, source, source_id, offence_code, code_description, offence_subcode, subcode_description)
-VALUES ('877323d1-1e0f-42f3-b5b5-f44b0e5bcb19', 'DELIUS', 1, '046', 'Stealing from shops and stalls (shoplifting)', '01', 'Stealing')
-    ON CONFLICT DO NOTHING;
+ON CONFLICT DO NOTHING;
 
 INSERT INTO assessed_episode (episode_uuid, user_id, created_date, end_date, change_reason, assessment_schema_code, assessment_uuid, answers, tables, offence_uuid)
 VALUES ('90f2b674-ae1c-488d-8b85-0251708ef6b6', 'AALONSO', '2021-01-01 08:11:53.177109', null, 'new episode', 'RSR', '6f3f2c4a-38ac-49ce-b790-70bc170fe553','{}','{}', '977323d1-1e0f-42f3-b5b5-f44b0e5bcb18')
