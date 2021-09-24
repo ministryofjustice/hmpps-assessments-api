97048cfb48ee15d5c261da1ffb85b827fcc53f4e
adelaidaa <adelaida.aj@gmail.com>
2021-09-24 16:54:38 +0100

    subject not sull

--- src/main/resources/db/migration/assessments/V1_1__assessment.sql
+++ src/main/resources/db/migration/assessments/V1_1__assessment.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS assessment
 (
     assessment_id           SERIAL          PRIMARY KEY,
     assessment_uuid         UUID            UNIQUE,
-    subject_uuid            UUID,
+    subject_uuid            UUID            NOT NULL,
     created_date            TIMESTAMP       NOT NULL,
     completed_date          TIMESTAMP,
     FOREIGN KEY (subject_uuid) REFERENCES subject (subject_uuid)
