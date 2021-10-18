d0ff44c49a81bff52d951b771b78d04d940834b4
adelaidaa <adelaida.aj@gmail.com>
2021-10-18 10:45:25 +0100

    klint

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/dto/QuestionSchemaDto.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.assessments.services.dto
-
-class ExternalSourceQuestionSchemaDto(
-  val questionCode: String,
-  val externalSource: String,
-  val jsonPathField: String,
-  val fieldType: String? = null,
-)
