b58e50d439e28f490b5f03df07e93c2ab4dd70eb
Lucas Cairns <lucas.cairns@gmail.com>
2021-11-01 16:21:25 +0000

    Fix question label for contact email (#365)

--- src/main/resources/db/migration/refdata/V1_7__upw_assessment.sql
+++ src/main/resources/db/migration/refdata/V1_7__upw_assessment.sql
@@ -121,7 +121,7 @@ VALUES
 ('c788ed5a-98e5-42a3-97f1-c6beefc324ee', 'contact_address_postcode', '2021-09-27 14:50:00', null, 'freetext', null, 'Postcode', null, null),
 ('3ce8f782-e8ca-48ff-9579-20b01dd79430', 'contact_phone_number', '2021-09-27 14:50:00', null, 'freetext', null, 'Phone number', null, null),
 ('2b4c0adf-547b-4d09-8be4-ea852f25ca8f', 'contact_mobile_phone_number', '2021-09-27 14:50:00', null, 'freetext', null, 'Phone number', null, null),
-('5732b512-97c1-47d5-b7b8-beb3d6cfdec6', 'contact_email_addresses', '2020-11-30 14:50:00', null, 'freetext', null, 'EMAIL', null, null),
+('5732b512-97c1-47d5-b7b8-beb3d6cfdec6', 'contact_email_addresses', '2020-11-30 14:50:00', null, 'freetext', null, 'Email', null, null),
 ('70eb1db7-4e68-4a59-b40c-7ce00f9c0aec', 'emergency_contact_name', '2021-09-27 14:50:00', null, 'freetext', null, 'Name', null, null),
 ('5805283b-d81e-4053-8e5f-b3d3247e7c58', 'emergency_contact_relationship', '2021-09-27 14:50:00', null, 'freetext', null, 'Relationship to the individual', null, null),
 ('9a2a0e8d-2e1c-45e6-84e8-e05c7d4f4b3a', 'emergency_contact_address_building_name', '2021-09-27 14:50:00', null, 'freetext', null, 'Building name', null, null),
