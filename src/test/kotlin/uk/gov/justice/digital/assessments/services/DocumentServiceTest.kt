c105df3995a00f5c9896c9effa18a736fe1b5ef6
Lucas Cairns <lucas.cairns@gmail.com>
2021-11-16 10:17:26 +0000

    Add additional test coverage for the document service

--- src/test/kotlin/uk/gov/justice/digital/assessments/services/DocumentServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/assessments/services/DocumentServiceTest.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.assessments.services
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.mock.web.MockMultipartFile
 import org.springframework.web.multipart.MultipartFile
@@ -15,6 +17,9 @@ import uk.gov.justice.digital.assessments.jpa.entities.assessments.AuthorEntity
 import uk.gov.justice.digital.assessments.jpa.entities.assessments.OffenceEntity
 import uk.gov.justice.digital.assessments.jpa.entities.assessments.SubjectEntity
 import uk.gov.justice.digital.assessments.restclient.CommunityApiRestClient
+import uk.gov.justice.digital.assessments.restclient.ExternalService
+import uk.gov.justice.digital.assessments.services.exceptions.EntityNotFoundException
+import uk.gov.justice.digital.assessments.services.exceptions.ExternalApiUnknownException
 import java.time.LocalDate
 import java.util.UUID
 
@@ -42,6 +47,54 @@ class DocumentServiceTest {
     documentService.uploadUpwDocument(assessmentId, episodeId, fileData)
   }
 
+  @Test
+  fun `throws when unable to find an assessment episode`() {
+    val episodeId = UUID.randomUUID()
+    val assessmentId = UUID.randomUUID()
+
+    val fileData = multiPartFile()
+
+    every { assessmentService.getEpisode(assessmentId, episodeId) } throws EntityNotFoundException("No episode")
+
+    assertThrows<EntityNotFoundException> {
+      documentService.uploadUpwDocument(assessmentId, episodeId, fileData)
+    }
+  }
+
+  @Test
+  fun `throws when the source ID does not exist for an offence`() {
+    val episodeId = UUID.randomUUID()
+    val assessmentId = UUID.randomUUID()
+    val crn = "X1234A"
+
+    val fileData = multiPartFile()
+
+    every { assessmentService.getEpisode(assessmentId, episodeId) } returns assessmentEpisode(crn)
+
+    assertThrows<EntityNotFoundException> {
+      documentService.uploadUpwDocument(assessmentId, episodeId, fileData)
+    }
+  }
+
+  @Test
+  fun `throws when unable to upload the PDF document`() {
+    val episodeId = UUID.randomUUID()
+    val assessmentId = UUID.randomUUID()
+    val crn = "X1234A"
+    val eventId = 456L
+    val convictionId = 987L
+
+    val fileData = multiPartFile()
+
+    every { assessmentService.getEpisode(assessmentId, episodeId) } returns assessmentEpisode(crn, eventId.toString())
+    every { offenderService.getOffenceFromConvictionIndex(crn, eventId) } returns OffenceDto(convictionId = convictionId)
+    every { communityApiRestClient.uploadDocumentToDelius(crn, convictionId, fileData) } throws ExternalApiUnknownException("Something went wrong", HttpMethod.POST, "/foo/bar", ExternalService.COMMUNITY_API)
+
+    assertThrows<ExternalApiUnknownException> {
+      documentService.uploadUpwDocument(assessmentId, episodeId, fileData)
+    }
+  }
+
   private fun multiPartFile(): MultipartFile {
     return MockMultipartFile(
       "file",
@@ -51,7 +104,7 @@ class DocumentServiceTest {
     )
   }
 
-  fun assessmentEpisode(crn: String, sourceId: String): AssessmentEpisodeEntity {
+  fun assessmentEpisode(crn: String, sourceId: String? = null): AssessmentEpisodeEntity {
     return AssessmentEpisodeEntity(
       assessment = AssessmentEntity(
         subject = SubjectEntity(
