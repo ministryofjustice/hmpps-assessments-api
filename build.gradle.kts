442a9960cede3a2fb61d98383bd80b33dcad7509
mark.chaplin <mark.heyward-chaplin@digital.justice.gov.uk>
2022-03-01 11:13:42 +0000

    ARN-897 Unsuppress Postgres warning as it's a library version warning not image (thanks Stuart)

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
   implementation("com.google.code.gson:gson:2.8.8")
   implementation("com.google.guava:guava:30.1.1-jre")
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("org.postgresql:postgresql")
+  implementation("org.postgresql:postgresql:42.3.2")
   implementation("org.flywaydb:flyway-core:7.15.0")
   runtimeOnly("com.h2database:h2:1.4.200")
 
