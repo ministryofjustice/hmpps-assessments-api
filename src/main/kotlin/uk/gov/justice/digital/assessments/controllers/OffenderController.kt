5823b4964f52481e3f00d55881527ca9feec15c9
cairnsp <101661307+cairnsp@users.noreply.github.com>
2022-03-29 10:11:42 +0100

    Addition of new rest endpoints for ARN-956 (#449)
    * Addition of new rest endpoints for ARN-956
    
    * Removal of timeout from integration test

--- src/main/kotlin/uk/gov/justice/digital/assessments/controllers/OffenderController.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/controllers/OffenderController.kt
@@ -1,34 +1,49 @@
 package uk.gov.justice.digital.assessments.controllers
 
-// TODO from ARN-618: Fix Offender service
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.assessments.api.DeliusEventType
+import uk.gov.justice.digital.assessments.api.OffenderDto
+import uk.gov.justice.digital.assessments.services.OffenderService
 
-//
-// import io.swagger.v3.oas.annotations.Operation
-// import io.swagger.v3.oas.annotations.Parameter
-// import io.swagger.v3.oas.annotations.responses.ApiResponse
-// import io.swagger.v3.oas.annotations.responses.ApiResponses
-// import org.springframework.web.bind.annotation.PathVariable
-// import org.springframework.web.bind.annotation.RequestMapping
-// import org.springframework.web.bind.annotation.RequestMethod
-// import org.springframework.web.bind.annotation.RestController
-// import uk.gov.justice.digital.assessments.api.OffenderDto
-// import uk.gov.justice.digital.assessments.services.OffenderService
-//
-// @RestController
-// class OffenderController(val offenderService: OffenderService) {
-//
-//   @RequestMapping(path = ["/offender/crn/{crn}/eventId/{eventId}"], method = [RequestMethod.GET])
-//   @Operation(description = "Gets offender and offence information")
-//   @ApiResponses(
-//     value = [
-//       ApiResponse(responseCode = "401", description = "Invalid JWT Token"),
-//       ApiResponse(responseCode = "200", description = "OK")
-//     ]
-//   )
-//   fun getOffenderAndOffence(
-//     @Parameter(description = "Offender CRN", required = true) @PathVariable crn: String,
-//     @Parameter(description = "Delius Event ID", required = true) @PathVariable eventId: Long
-//   ): OffenderDto? {
-//     return offenderService.getOffenderAndOffence(crn, eventId)
-//   }
-// }
+@RestController
+class OffenderController(val offenderService: OffenderService) {
+
+  companion object {
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+  }
+
+  @RequestMapping(
+    path = [
+      "/offender/crn/{crn}/eventId/{eventId}",
+      "/offender/crn/{crn}/eventType/{eventType}/eventId/{eventId}"
+    ],
+    method = [RequestMethod.GET]
+  )
+  @Operation(description = "Get offender information")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "401", description = "Invalid JWT Token"),
+      ApiResponse(responseCode = "200", description = "OK")
+    ]
+  )
+  fun getOffenderDetails(
+    @Parameter(description = "Offender CRN", required = true) @PathVariable crn: String,
+    @Parameter(description = "Delius Event type", required = false) @PathVariable eventType: DeliusEventType?,
+    @Parameter(description = "Delius Event ID", required = true) @PathVariable eventId: Long
+  ): OffenderDto? {
+    log.debug("Entered getOffenderDetails($crn, $eventType, $eventId)")
+
+    val offenderDto = offenderService.getOffender(crn)
+    offenderDto.offence = offenderService.getOffence(eventType, crn, eventId)
+    return offenderDto
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/assessments/controller/OffenderControllerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/assessments/controller/OffenderControllerTest.kt
@@ -1,102 +1,111 @@
 package uk.gov.justice.digital.assessments.controller
 
-// TODO from ARN-618: Fix the offender and offence work
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+import uk.gov.justice.digital.assessments.api.DeliusEventType
+import uk.gov.justice.digital.assessments.api.OffenceDto
+import uk.gov.justice.digital.assessments.api.OffenderDto
+import uk.gov.justice.digital.assessments.testutils.IntegrationTest
+import java.time.LocalDate
 
-//
-// import org.assertj.core.api.Assertions.assertThat
-// import org.junit.jupiter.api.Test
-// import org.springframework.test.context.jdbc.Sql
-// import org.springframework.test.context.jdbc.SqlConfig
-// import org.springframework.test.context.jdbc.SqlGroup
-// import org.springframework.test.web.reactive.server.expectBody
-// import uk.gov.justice.digital.assessments.api.OffenderDto
-// import uk.gov.justice.digital.assessments.testutils.IntegrationTest
-//
-// @SqlGroup(
-//  Sql(scripts = ["classpath:subject/before-test.sql"], config = SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED)),
-//  Sql(scripts = ["classpath:subject/after-test.sql"], config = SqlConfig(transactionMode = SqlConfig.TransactionMode.ISOLATED), executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-// )
-// class OffenderControllerTest : IntegrationTest() {
-//
-//  val crn = "DX12340A"
-//  val eventId = 1
-//  val oasysUserId = 101L
-//  @Test
-//  fun `access forbidden when no authority`() {
-//    webTestClient.get().uri("/offender/crn/$crn")
-//      .header("Content-Type", "application/json")
-//      .exchange()
-//      .expectStatus().isUnauthorized
-//  }
-//
-//  @Test
-//  fun `get offender returns offender and offence for crn and conviction ID`() {
-//    val offenderDto = webTestClient.get().uri("/offender/crn/$crn/eventId/$eventId")
-//      .headers(setAuthorisation())
-//      .exchange()
-//      .expectStatus().isOk
-//      .expectBody<OffenderDto>()
-//      .returnResult()
-//      .responseBody
-//
-//    assertThat(offenderDto?.offenderId).isEqualTo(oasysUserId)
-//    assertThat(offenderDto?.offence?.offenceCode).isEqualTo("116")
-//    assertThat(offenderDto?.offence?.offenceSubCode).isEqualTo("00")
-//  }
-//
-//  @Test
-//  fun `returns offender with address`() {
-//    val offenderDto = webTestClient.get().uri("/offender/crn/$crn/eventId/$eventId")
-//      .headers(setAuthorisation())
-//      .exchange()
-//      .expectStatus().isOk
-//      .expectBody<OffenderDto>()
-//      .returnResult()
-//      .responseBody
-//
-//    assertThat(offenderDto?.offenderId).isEqualTo(oasysUserId)
-//    assertThat(offenderDto?.address?.address1).isEqualTo("38")
-//    assertThat(offenderDto?.address?.postcode).isEqualTo("ad21 5dr")
-//  }
-//
-//  @Test
-//  fun `returns not found for invalid crn`() {
-//    val invalidCrn = "invalidNotFound"
-//    webTestClient.get().uri("/offender/crn/$invalidCrn/eventId/$eventId")
-//      .headers(setAuthorisation())
-//      .exchange()
-//      .expectStatus().isNotFound
-//  }
-//
-//  @Test
-//  fun `returns offender with aliases`() {
-//    val offenderDto = webTestClient.get().uri("/offender/crn/$crn/eventId/$eventId")
-//      .headers(setAuthorisation())
-//      .exchange()
-//      .expectStatus().isOk
-//      .expectBody<OffenderDto>()
-//      .returnResult()
-//      .responseBody
-//
-//    assertThat(offenderDto?.offenderId).isEqualTo(oasysUserId)
-//    assertThat(offenderDto?.firstNameAliases).containsOnly("John", "Jonny")
-//    assertThat(offenderDto?.surnameAliases).containsOnly("Smithy")
-//  }
-//
-//  @Test
-//  fun `returns offence with category codes and code descriptions`() {
-//    val offenderDto = webTestClient.get().uri("/offender/crn/$crn/eventId/$eventId")
-//      .headers(setAuthorisation())
-//      .exchange()
-//      .expectStatus().isOk
-//      .expectBody<OffenderDto>()
-//      .returnResult()
-//      .responseBody
-//
-//    assertThat(offenderDto?.offenderId).isEqualTo(oasysUserId)
-//    assertThat(offenderDto?.offence?.offenceCode).isEqualTo("116")
-//    assertThat(offenderDto?.offence?.codeDescription).isEqualTo("Fishery Laws")
-//    assertThat(offenderDto?.offence?.offenceSubCode).isEqualTo("00")
-//    assertThat(offenderDto?.offence?.subCodeDescription).isEqualTo("Fishery Laws")
-//  }
-// }
+@AutoConfigureWebTestClient
+internal class OffenderControllerTest : IntegrationTest() {
+
+  @Test
+  fun `should handle request when EventType is absent`() {
+
+    val crn = "DX5678A"
+    val eventId = 1
+    val path = "/offender/crn/$crn/eventId/$eventId"
+
+    val offenderDto = webTestClient.get().uri(path)
+      .headers(setAuthorisation(roles = listOf("ROLE_PROBATION")))
+      .exchange()
+      .expectStatus().isOk
+      .expectBody<OffenderDto>()
+      .returnResult()
+      .responseBody
+
+    Assertions.assertThat(offenderDto).isEqualTo(
+      OffenderDto(
+        offenderId = 101,
+        firstName = "John",
+        surname = "Smith",
+        dateOfBirth = LocalDate.of(1979, 8, 18),
+        gender = "Male",
+        crn = crn,
+        pncNumber = "A/1234560BA",
+        firstNameAliases = listOf("John", "Jonny"),
+        surnameAliases = listOf("Smithy"),
+        offence = OffenceDto(
+          convictionId = 2500000223,
+          convictionIndex = 1,
+          offenceCode = "046",
+          codeDescription = "Stealing from shops and stalls (shoplifting)",
+          offenceSubCode = "00",
+          subCodeDescription = "Stealing from shops and stalls (shoplifting)",
+          sentenceDate = LocalDate.of(2014, 8, 25)
+        )
+      )
+    )
+  }
+
+  @ParameterizedTest(name = "Delius Event type = {0}")
+  @MethodSource("getConvictionData")
+  fun `should return offence details Dto given provided parameters`(
+    eventType: DeliusEventType,
+    expectedConvictionId: Long,
+    eventId: Long
+  ) {
+
+    val crn = "DX5678A"
+    val path = "/offender/crn/$crn/eventType/$eventType/eventId/$eventId"
+
+    val offenderDto = webTestClient.get().uri(path)
+      .headers(setAuthorisation(roles = listOf("ROLE_PROBATION")))
+      .exchange()
+      .expectStatus().isOk
+      .expectBody<OffenderDto>()
+      .returnResult()
+      .responseBody
+
+    Assertions.assertThat(offenderDto).isEqualTo(
+      OffenderDto(
+        offenderId = 101,
+        firstName = "John",
+        surname = "Smith",
+        dateOfBirth = LocalDate.of(1979, 8, 18),
+        gender = "Male",
+        crn = crn,
+        pncNumber = "A/1234560BA",
+        firstNameAliases = listOf("John", "Jonny"),
+        surnameAliases = listOf("Smithy"),
+        offence = OffenceDto(
+          convictionId = expectedConvictionId,
+          convictionIndex = 1,
+          offenceCode = "046",
+          codeDescription = "Stealing from shops and stalls (shoplifting)",
+          offenceSubCode = "00",
+          subCodeDescription = "Stealing from shops and stalls (shoplifting)",
+          sentenceDate = LocalDate.of(2014, 8, 25)
+        )
+      )
+    )
+  }
+
+  companion object {
+    @JvmStatic
+    fun getConvictionData(): List<Arguments> {
+
+      return listOf(
+        Arguments.of(DeliusEventType.EVENT_INDEX, 2500000223, 1),
+        Arguments.of(DeliusEventType.EVENT_ID, 123456, 123456),
+      )
+    }
+  }
+}
