f821f01bc11f764aaa88e03dc550d73bf9212850
nataliewood <natalie.wood@digital.justice.gov.uk>
2022-04-04 18:02:12 +0100

    :construction: first pass on updating disabilities and adding multiples

---
+++ src/main/kotlin/uk/gov/justice/digital/assessments/restclient/communityapi/DeliusDisabilityDto.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.assessments.restclient.communityapi
+
+data class DeliusDisabilityDto(
+    val disabilityType: DisabilityType,
+    val notes: String? = null,
+    val provisions: List<Provision>? = null,
+)
+
+data class DisabilityType(
+  val code: String,
+  val description: String
+)
+
+class Provision (
+  val notes: String? = null,
+  val provisionType: ProvisionType? = null
+)
+
+data class ProvisionType(
+  val code: String,
+  val description: String
+)
