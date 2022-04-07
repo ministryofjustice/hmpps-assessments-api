8121b3f12c4d05e5bdc0d3ee5af09b9b00a6297e
mark.chaplin <mark.heyward-chaplin@digital.justice.gov.uk>
2022-04-07 12:05:40 +0100

    ARN-948 Add gp practice name

---
+++ src/main/resources/db/migration/refdata/V1_12__gp_details_add_medical_practice.sql
@@ -0,0 +1,4 @@
+INSERT INTO question_schema (question_schema_uuid, question_code, question_start, question_end, answer_type, answer_schema_group_uuid, question_text, question_help_text, reference_data_category)
+VALUES
+    ('6001121c-5b82-467f-bcfe-597f5e91e2f0', 'gp_practice_name', '2021-09-27 14:50:00', null, 'freetext', null, 'Medical practice', null, null)
+;
