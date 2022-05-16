09888796d1ed178c1b40829db6f06d364efdb61c
Paul Coupe <paul.coupe@digital.justice.gov.uk>
2022-05-16 11:38:46 +0100

    Arn 935 rename flyway file (#477)
    * ARN-935 renames clashing flyway filename
    
    * ARN-935 update flyway file to refer to renamed table and field names

--- src/main/resources/db/migration/refdata/V1_15__update_culture_religious_question_text.sql
+++
@@ -1,2 +0,0 @@
--- Update question text from culture and religious adjustments
-UPDATE question_schema SET question_text = 'Are adjustments required for cultural or religious reasons?' WHERE question_schema_uuid = '5cefd241-cc51-4128-a343-cb7c438a9048';
