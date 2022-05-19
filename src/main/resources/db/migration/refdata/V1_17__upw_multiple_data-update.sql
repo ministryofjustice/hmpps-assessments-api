30c80e691cf081cc6d6efb9f3f2b5d7e9f808397
nataliewood <natalie.wood@digital.justice.gov.uk>
2022-05-19 14:57:09 +0100

    fix flyway

--- src/main/resources/db/migration/refdata/V1_17__upw_multiple_data-update.sql
+++ src/main/resources/db/migration/refdata/V1_17__upw_multiple_data-update.sql
@@ -23,13 +23,13 @@ WHERE external_source_question_mapping_uuid in
     '7762598e-dc3c-41a4-8546-38444c0ef708',
     '7802580e-fa85-4998-966e-cc29e58f04a5');
 
-INSERT INTO question (question_uuid, question_code, question_start, question_end, answer_type, answer_schema_group_uuid, question_text, question_help_text, reference_data_category)
+INSERT INTO question (question_uuid, question_code, question_start, question_end, answer_type, answer_group_uuid, question_text, question_help_text, reference_data_category)
 VALUES
     ('b8f51e03-c2d6-4ec5-b539-30ba94524d47', 'emergency_contact_details', '2021-09-27 14:50:00', null, 'table', null, '', null, null),
     ('2a1a41dd-5d77-4674-9f8f-561c654867b8', 'gp_details', '2021-09-27 14:50:00', null, 'table', null, '', '', null);
 
 
-INSERT INTO external_source_question_mapping (external_source_question_mapping_uuid, question_code, assessment_schema_code, external_source, json_path_field, field_type, external_source_endpoint, mapped_value, if_empty)
+INSERT INTO external_source_question_mapping (external_source_question_mapping_uuid, question_code, assessment_code, external_source, json_path_field, field_type, external_source_endpoint, mapped_value, if_empty)
 VALUES
     ('8e219d36-6979-4018-8944-12777990910e', 'emergency_contact_details', 'UPW', 'DELIUS', '$[?(@.relationshipType.code==''ME''&&@.isActive==true)]', 'structured', 'secure/offenders/crn/$crn/personalContacts', null, false),
     ('cb24dc64-e8fd-4960-8096-1907d59de3b0', 'gp_details', 'UPW', 'DELIUS', '$[?(@.relationshipType.code==''RT02''&&@.isActive==true)]', 'structured', 'secure/offenders/crn/$crn/personalContacts', null, false);
