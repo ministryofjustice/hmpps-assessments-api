3942b3b90f02b287782d9832088a54f66ef6d0af
Lucas Cairns <lucas.cairns@gmail.com>
2022-07-20 16:10:04 +0100

    Update checkbox labels on the UPW availability page (#513)

---
+++ src/main/resources/db/migration/refdata/V1_23__upw_update_availability_checkbox_labels.sql
@@ -0,0 +1,28 @@
+-- Update checkbox labels for UPW availability
+UPDATE answer SET text = 'Available for work Monday morning' WHERE answer_code = 'monday_morning';
+UPDATE answer SET text = 'Available for work Monday afternoon' WHERE answer_code = 'monday_afternoon';
+UPDATE answer SET text = 'Available for work Monday evening' WHERE answer_code = 'monday_evening';
+
+UPDATE answer SET text = 'Available for work Tuesday morning' WHERE answer_code = 'tuesday_morning';
+UPDATE answer SET text = 'Available for work Tuesday afternoon' WHERE answer_code = 'tuesday_afternoon';
+UPDATE answer SET text = 'Available for work Tuesday evening' WHERE answer_code = 'tuesday_evening';
+
+UPDATE answer SET text = 'Available for work Wednesday morning' WHERE answer_code = 'wednesday_morning';
+UPDATE answer SET text = 'Available for work Wednesday afternoon' WHERE answer_code = 'wednesday_afternoon';
+UPDATE answer SET text = 'Available for work Wednesday evening' WHERE answer_code = 'wednesday_evening';
+
+UPDATE answer SET text = 'Available for work Thursday morning' WHERE answer_code = 'thursday_morning';
+UPDATE answer SET text = 'Available for work Thursday afternoon' WHERE answer_code = 'thursday_afternoon';
+UPDATE answer SET text = 'Available for work Thursday evening' WHERE answer_code = 'thursday_evening';
+
+UPDATE answer SET text = 'Available for work Friday morning' WHERE answer_code = 'friday_morning';
+UPDATE answer SET text = 'Available for work Friday afternoon' WHERE answer_code = 'friday_afternoon';
+UPDATE answer SET text = 'Available for work Friday evening' WHERE answer_code = 'friday_evening';
+
+UPDATE answer SET text = 'Available for work Saturday morning' WHERE answer_code = 'saturday_morning';
+UPDATE answer SET text = 'Available for work Saturday afternoon' WHERE answer_code = 'saturday_afternoon';
+UPDATE answer SET text = 'Available for work Saturday evening' WHERE answer_code = 'saturday_evening';
+
+UPDATE answer SET text = 'Available for work Sunday morning' WHERE answer_code = 'sunday_morning';
+UPDATE answer SET text = 'Available for work Sunday afternoon' WHERE answer_code = 'sunday_afternoon';
+UPDATE answer SET text = 'Available for work Sunday evening' WHERE answer_code = 'sunday_evening';
