659e4676020de19febc301134b708198e5fa6479
Lucas Cairns <lucas.cairns@gmail.com>
2022-09-16 11:26:45 +0100

    Add configuration to remove orphaned additional details answers

--- src/test/kotlin/uk/gov/justice/digital/assessments/utils/AssessmentUtilsTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/assessments/utils/AssessmentUtilsTest.kt
@@ -64,4 +64,69 @@ class AssessmentUtilsTest {
     assertThat(episode.answers["placement_preference_complete"]).isNull()
     assertThat(episode.answers["some_other_question"]).isEqualTo(listOf("Should not be removed"))
   }
+
+  @Test
+  fun `should support questions with multiple dependents`() {
+    val episode =
+      AssessmentEpisodeEntity(
+        episodeId = 1,
+        assessmentType = AssessmentType.UPW,
+        author = authorEntity,
+        assessment = AssessmentEntity(),
+        endDate = LocalDateTime.now().minusDays(1),
+        answers = mutableMapOf(
+          "employment_education" to listOf("PARTTIME_EDUCATION_EMPLOYMENT"),
+          "employment_education_details_fulltime" to listOf("Should be removed"),
+          "employment_education_details_parttime" to listOf("Should not be removed"),
+        )
+      )
+
+    AssessmentUtils.removeOrphanedAnswers(episode)
+
+    Assertions.assertThat(episode.answers["employment_education"]).isEqualTo(listOf("PARTTIME_EDUCATION_EMPLOYMENT"))
+    Assertions.assertThat(episode.answers["employment_education_details_fulltime"]).isNull()
+    Assertions.assertThat(episode.answers["employment_education_details_parttime"]).isEqualTo(listOf("Should not be removed"))
+  }
+
+  @Test
+  fun `should keep details when there are carer commitments`() {
+    val episode =
+      AssessmentEpisodeEntity(
+        episodeId = 1,
+        assessmentType = AssessmentType.UPW,
+        author = authorEntity,
+        assessment = AssessmentEntity(),
+        endDate = LocalDateTime.now().minusDays(1),
+        answers = mutableMapOf(
+          "active_carer_commitments" to listOf("Should not be removed"),
+          "active_carer_commitments_details" to listOf("Should not be removed"),
+        )
+      )
+
+    AssessmentUtils.removeOrphanedAnswers(episode)
+
+    Assertions.assertThat(episode.answers["active_carer_commitments"]).isEqualTo(listOf("Should not be removed"))
+    Assertions.assertThat(episode.answers["active_carer_commitments_details"]).isEqualTo(listOf("Should not be removed"))
+  }
+
+  @Test
+  fun `should not keep details when there are no carer commitments`() {
+    val episode =
+      AssessmentEpisodeEntity(
+        episodeId = 1,
+        assessmentType = AssessmentType.UPW,
+        author = authorEntity,
+        assessment = AssessmentEntity(),
+        endDate = LocalDateTime.now().minusDays(1),
+        answers = mutableMapOf(
+          "active_carer_commitments" to emptyList(),
+          "active_carer_commitments_details" to listOf("Should be removed"),
+        )
+      )
+
+    AssessmentUtils.removeOrphanedAnswers(episode)
+
+    Assertions.assertThat(episode.answers["active_carer_commitments"]).isEqualTo(emptyList<String>())
+    Assertions.assertThat(episode.answers["active_carer_commitments_details"]).isNull()
+  }
 }
