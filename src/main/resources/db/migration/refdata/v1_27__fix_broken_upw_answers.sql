a97c4d82e5a1486cb4426fc54374a484f305d039
Lucas Cairns <lucas.cairns@gmail.com>
2022-09-21 17:43:19 +0100

    Add missing answers to the yes/no answergroup

---
+++ src/main/resources/db/migration/refdata/v1_27__fix_broken_upw_answers.sql
@@ -0,0 +1,3 @@
+INSERT INTO answer (answer_uuid, answer_code, answer_group_uuid, answer_start, answer_end, value, text)
+VALUES ('b051e8ff-1a24-417c-9fc8-fe0c6fd16565', 'yes', '887f4528-06d1-4247-8bc3-5e679222baa6', '2022-09-21 01:00:00', null, 'YES', 'Yes'),
+       ('23e85ca3-689b-42e9-a53d-9e226e795393', 'no', '887f4528-06d1-4247-8bc3-5e679222baa6', '2022-09-21 01:00:00', null, 'NO', 'No');
