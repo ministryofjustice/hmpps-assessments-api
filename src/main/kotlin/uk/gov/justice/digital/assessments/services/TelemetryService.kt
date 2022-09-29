1cdf9d45db16997b299e084854866870400c5ee7
nataliewood <natalie.wood@digital.justice.gov.uk>
2022-09-29 17:20:04 +0100

    add audit and logging event for cloning assessment

--- src/main/kotlin/uk/gov/justice/digital/assessments/services/TelemetryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/services/TelemetryService.kt
@@ -4,6 +4,7 @@ import com.microsoft.applicationinsights.TelemetryClient
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.assessments.jpa.entities.AssessmentType
 import uk.gov.justice.digital.assessments.jpa.entities.assessments.AuthorEntity
+import java.time.LocalDateTime
 import java.util.UUID
 
 @Service
@@ -11,15 +12,49 @@ class TelemetryService(
   private val telemetryClient: TelemetryClient
 ) {
 
-  fun trackAssessmentEvent(event: TelemetryEventType, crn: String?, author: AuthorEntity, assessmentUuid: UUID, episodeUuid: UUID, assessmentType: AssessmentType) {
-    val properties = mapOf(
+  fun trackAssessmentEvent(
+    event: TelemetryEventType,
+    crn: String?,
+    author: AuthorEntity,
+    assessmentUuid: UUID,
+    episodeUuid: UUID,
+    assessmentType: AssessmentType,
+    additionalFields: Map<String, String>? = null
+  ) {
+    val properties = mutableMapOf(
       "author" to author.userName,
       "crn" to crn,
       "assessmentUUID" to assessmentUuid.toString(),
       "episodeUUID" to episodeUuid.toString(),
       "assessmentType" to assessmentType.name
     )
+    if (additionalFields != null) {
+      properties.putAll(additionalFields)
+    }
     telemetryClient.trackEvent(event.eventName, properties, emptyMap())
+  }
+
+  fun trackAssessmentClonedEvent(
+    crn: String?,
+    author: AuthorEntity,
+    assessmentUuid: UUID,
+    episodeUuid: UUID,
+    assessmentType: AssessmentType,
+    clonedFromEpisodeUUID: UUID,
+    clonedFromCompletedDate: LocalDateTime
+  ) {
+    trackAssessmentEvent(
+      TelemetryEventType.ASSESSMENT_CLONED,
+      crn,
+      author,
+      assessmentUuid,
+      episodeUuid,
+      assessmentType,
+      mapOf(
+        "mostRecentEpisodeUUID" to clonedFromEpisodeUUID.toString(),
+        "mostRecentEpisodeCompletedDate" to clonedFromCompletedDate.toString()
+      )
+    )
   }
 }
 
@@ -27,5 +62,7 @@ enum class TelemetryEventType(val eventName: String) {
   ASSESSMENT_CREATED("arnAssessmentCreated"),
   ASSESSMENT_CLOSED("arnAssessmentClosed"),
   ASSESSMENT_REALLOCATED("arnAssessmentReallocated"),
-  ASSESSMENT_COMPLETE("arnAssessmentCompleted")
+  ASSESSMENT_COMPLETE("arnAssessmentCompleted"),
+  ASSESSMENT_CLONED("arnAssessmentCloned")
 }
+
