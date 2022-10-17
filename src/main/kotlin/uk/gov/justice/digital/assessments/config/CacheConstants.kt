6ee36c1c646df0756e8f45f545b1ea7bc79937e5
Natalie Wood <Natalie.wood@digital.justice.gov.uk>
2022-10-17 12:18:35 +0100

    update the ref data cache keys (#558)

--- src/main/kotlin/uk/gov/justice/digital/assessments/config/CacheConstants.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/config/CacheConstants.kt
@@ -2,11 +2,11 @@ package uk.gov.justice.digital.assessments.config
 
 object CacheConstants {
 
-  const val ASSESSMENT_CACHE_KEY: String = "AssessmentService:assessment"
-  const val QUESTIONS_FOR_ASSESSMENT_TYPE_CACHE_KEY: String = "AssessmentService:questionsForAssessmentType"
-  const val ASSESSMENT_SUMMARY_CACHE_KEY: String = "AssessmentService:assessmentSummary"
-  const val QUESTION_CACHE_KEY: String = "QuestionService:getQuestion"
-  const val LIST_QUESTION_GROUPS_CACHE_KEY: String = "QuestionService:listGroups"
-  const val QUESTION_GROUP_CONTENTS_CACHE_KEY: String = "QuestionService:getGroupContents"
-  const val QUESTION_GROUP_SECTIONS_CACHE_KEY: String = "QuestionService:getGroupSections"
+  const val ASSESSMENT_CACHE_KEY: String = "AssessmentService:assessment1"
+  const val QUESTIONS_FOR_ASSESSMENT_TYPE_CACHE_KEY: String = "AssessmentService:questionsForAssessmentType1"
+  const val ASSESSMENT_SUMMARY_CACHE_KEY: String = "AssessmentService:assessmentSummary1"
+  const val QUESTION_CACHE_KEY: String = "QuestionService:getQuestion1"
+  const val LIST_QUESTION_GROUPS_CACHE_KEY: String = "QuestionService:listGroups1"
+  const val QUESTION_GROUP_CONTENTS_CACHE_KEY: String = "QuestionService:getGroupContents1"
+  const val QUESTION_GROUP_SECTIONS_CACHE_KEY: String = "QuestionService:getGroupSections1"
 }
