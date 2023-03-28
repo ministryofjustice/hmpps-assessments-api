b62a941b4f487290d92f93a201feccb1c0e0766b
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-03-28 16:39:41 +0100

    remove redundant DeliusDisabilityDto.kt and PersonalDetails.kt

--- src/main/kotlin/uk/gov/justice/digital/assessments/api/answers/DisabilityAnswerDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/api/answers/DisabilityAnswerDto.kt
@@ -1,10 +1,7 @@
 package uk.gov.justice.digital.assessments.api.answers
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import uk.gov.justice.digital.assessments.restclient.communityapi.DeliusDisabilityDto
-import uk.gov.justice.digital.assessments.restclient.communityapi.Provision
 import uk.gov.justice.digital.assessments.restclient.deliusintegrationapi.Disability
-import java.time.LocalDate
 
 data class DisabilityAnswerDto(
   @JsonProperty("code")
@@ -21,25 +18,6 @@ data class DisabilityAnswerDto(
 ) {
   companion object {
 
-    fun from(deliusDisabilities: List<DeliusDisabilityDto>): List<DisabilityAnswerDto> {
-      return if (deliusDisabilities.isEmpty()) emptyList()
-      else deliusDisabilities.map { from(it) }
-    }
-
-    private fun whereActive(provision: Provision): Boolean {
-      val now = LocalDate.now()
-      return (provision.startDate.isBefore(now) || provision.startDate.isEqual(now)) &&
-        (provision.finishDate == null || provision.finishDate.isAfter(now))
-    }
-
-    fun from(deliusDisability: DeliusDisabilityDto): DisabilityAnswerDto {
-      return DisabilityAnswerDto(
-        code = deliusDisability.disabilityType.code,
-        description = deliusDisability.disabilityType.description,
-        notes = deliusDisability.notes,
-        adjustments = deliusDisability.provisions?.filter(this::whereActive)?.map { it.provisionType?.description }
-      )
-    }
     fun from(deliusDisability: Disability): DisabilityAnswerDto {
       return DisabilityAnswerDto(
         code = deliusDisability.type.code,
--- src/main/kotlin/uk/gov/justice/digital/assessments/restclient/deliusintegrationapi/PersonalDetails.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.assessments.restclient.deliusintegrationapi
-
-data class PersonalDetails(
-  val crn: String,
-  val personalCircumstances: List<PersonalCircumstance>,
-  val personalContacts: List<PersonalContact>
-)
