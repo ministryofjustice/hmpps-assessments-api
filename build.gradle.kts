e11654e7637a0dc1578cfcd188dd8f23d53dea15
nataliewood <natalie.wood@digital.justice.gov.uk>
2023-04-17 10:21:51 +0100

    add toolchain to gradle to keep java and kotlin versions aligned

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,3 +61,13 @@ dependencies {
 repositories {
   mavenCentral()
 }
+java {
+  toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+tasks {
+  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+      jvmTarget = "17"
+    }
+  }
+}
