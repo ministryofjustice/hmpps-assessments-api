7eacdac170097af6fc3a5358603df3df59e2f1de
Lucas Cairns <lucas.cairns@gmail.com>
2023-06-07 16:18:30 +0100

    Add DTO classes for the new disability structure

--- src/main/kotlin/uk/gov/justice/digital/assessments/api/answers/DisabilityAnswerDto.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/api/answers/DisabilityAnswerDto.kt
@@ -1,30 +1,17 @@
 package uk.gov.justice.digital.assessments.api.answers
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import uk.gov.justice.digital.assessments.restclient.deliusintegrationapi.Disability
 
 data class DisabilityAnswerDto(
-  @JsonProperty("code")
-  val code: String? = null,
-
-  @JsonProperty("description")
-  val description: String? = null,
-
-  @JsonProperty("disability_notes")
+  val type: TypeDto,
+  val condition: TypeDto,
   val notes: String? = null,
-
-  @JsonProperty("disability_adjustments")
-  val adjustments: List<String?>? = null,
 ) {
   companion object {
-
-    fun from(deliusDisability: Disability): DisabilityAnswerDto {
-      return DisabilityAnswerDto(
-        code = deliusDisability.type.code,
-        description = deliusDisability.type.description,
-        notes = deliusDisability.notes,
-        adjustments = deliusDisability.provisions,
-      )
-    }
+    fun from(disability: Disability) = DisabilityAnswerDto(
+      type = TypeDto.from(disability.type),
+      condition = TypeDto.from(disability.condition),
+      notes = disability.notes,
+    )
   }
 }
