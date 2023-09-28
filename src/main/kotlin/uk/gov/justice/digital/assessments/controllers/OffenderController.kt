b4e381785d5b65e9ad5e908550190bbdc46a1928
Lucas Cairns <lucas.cairns@gmail.com>
2023-09-28 12:49:58 +0100

    Add missing PreAuthorize annotation to getOffenderDetails endpoint

--- src/main/kotlin/uk/gov/justice/digital/assessments/controllers/OffenderController.kt
+++ src/main/kotlin/uk/gov/justice/digital/assessments/controllers/OffenderController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
@@ -33,6 +34,7 @@ class OffenderController(val offenderService: OffenderService) {
       ApiResponse(responseCode = "200", description = "OK"),
     ],
   )
+  @PreAuthorize("hasRole('ROLE_PROBATION')")
   fun getOffenderDetails(
     @Parameter(description = "Offender CRN", required = true) @PathVariable crn: String,
     @Parameter(description = "Delius Event ID", required = true) @PathVariable eventId: Long,
