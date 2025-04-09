83c64dcb034acfe136b5c102b6bc6eb94253b89c
Lucas Cairns <lucas.cairns@gmail.com>
2025-04-09 17:32:59 +0100

    Fix assertions

--- src/test/kotlin/uk/gov/justice/digital/assessments/repositories/QuestionGroupRepositoryTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/assessments/repositories/QuestionGroupRepositoryTest.kt
@@ -24,7 +24,7 @@ class QuestionGroupRepositoryTest(@Autowired val questionGroupRepository: Questi
   @Test
   fun `list group summaries`() {
     val groupSummaries = questionGroupRepository.listGroups()
-    assertThat(groupSummaries).hasSize(27)
+    assertThat(groupSummaries).hasSize(28)
 
     val groupInfo = groupSummaries.find { it.groupCode == "placement_gender_preferences" }
 
