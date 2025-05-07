ef7d22662a986c7966a4c24c82d2f61a07ab2b28
Lucas Cairns <lucas.cairns@gmail.com>
2025-05-07 14:59:18 +0100

    Fix incorrect column name in migration v1.35

--- src/main/resources/db/migration/refdata/V1_35__add_additional_questions_to_clone_exclusion.sql
+++ src/main/resources/db/migration/refdata/V1_35__add_additional_questions_to_clone_exclusion.sql
@@ -1,2 +1,2 @@
-INSERT INTO clone_assessment_excluded_questions(assessment_schema_code, question_code)
+INSERT INTO clone_assessment_excluded_questions(assessment_type, question_code)
 VALUES ('UPW', 'other_adjustments_complete');
